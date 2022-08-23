e180120f1773183df3e43e6c7f9a113efecbff62
Claudio Carigé <ccarige@gmail.com>
2022-08-22 23:57:14 -0300

    Contagem de post nas páginas Usuários e Perfil

--- mastertechnology/models.py
+++ mastertechnology/models.py
@@ -2,7 +2,6 @@ from mastertechnology import db, login_usuario
 from datetime import datetime
 from flask_login import UserMixin
 
-
 '''  
      *   Classes dos Formulários  
 '''
@@ -14,7 +13,6 @@ def load_usuario(id_usuario):
 
 
 class Usuarios(db.Model, UserMixin):
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
@@ -23,12 +21,13 @@ class Usuarios(db.Model, UserMixin):
     postagens = db.relationship('Postagens', backref='autor', lazy=True)
     linguagem = db.Column(db.String, nullable=False, default="Não informado")
 
+    def contagem_post(self):
+        return len(self.postagens)
+
 
 class Postagens(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String, nullable=False)
     corpo_post = db.Column(db.Text, nullable=False)
     data_criacao = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     id_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
-
